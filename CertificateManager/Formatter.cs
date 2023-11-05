00c4eac5c15260bdb3f1bb7c05b943252fa0fbd1
andrea1312 <andreacukic55@gmail.com>
2023-11-05 22:25:39 +0100

    napravljena klasa Formatter

---
+++ CertificateManager/Formatter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Manager
+{
+    public class Formatter
+    {
+        //Returns username based on the Windows Logon Name. 
+        public static string ParseName(string winLogonName)
+        {
+            string[] parts = new string[] { };
+
+            if (winLogonName.Contains("@"))
+            {
+                //UPN format
+                parts = winLogonName.Split('@');
+                return parts[0];
+            }
+            else if (winLogonName.Contains("\\"))
+            {
+                //SPN format
+                parts = winLogonName.Split('\\');
+                return parts[1];
+            }
+            else
+            {
+                return winLogonName;
+            }
+        }
+    }
+}
