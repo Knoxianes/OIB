6cf4c66c9115e30ad172bd004f04a4555e2f6df0
andrea1312 <andreacukic55@gmail.com>
2023-11-05 22:23:34 +0100

    napravljena klasa ClientCertValidator

---
+++ CertificateManager/ClientCertValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IdentityModel.Selectors;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Manager
+{
+    public class ClientCertValidator : X509CertificateValidator
+    {
+        //Implementation of a custom certificate validation on the client side.
+        public override void Validate(X509Certificate2 certificate)
+        {
+            if (certificate.Subject.Equals(certificate.Issuer))
+            {
+                throw new Exception("Certificate is self-issued.");
+            }
+        }
+    }
+}
