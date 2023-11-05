94a0ebcb5e9d29ab7f4d33df5dfb94fe2c0e9811
Andrea Cukic <cukic.pr145.2020@uns.ac.rs>
2023-11-05 21:09:33 +0100

    autentifikacija klijenta

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,10 +16,15 @@ namespace Client
 
             NetTcpBinding binding = new NetTcpBinding();
             EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:4000/IProcessServis"));
+
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
             using (WCFClient proxy = new WCFClient(binding, address))
             {
-                
-                Console.WriteLine("Client Started");
+
+                Console.WriteLine("Client Started > " + WindowsIdentity.GetCurrent().Name);
                 Console.ReadLine();
             }
         }
