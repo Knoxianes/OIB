108976446579b1ef1b705aa5cca101fee59af6c2
Katarina Marinkovic <ninikaca@gmail.com>
2023-11-27 20:30:09 +0100

    custom principal

--- SecurityManager/CustomPrincipal.cs
+++ SecurityManager/CustomPrincipal.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SecurityManager
 {
-    internal class CustomPrincipal
+    class CustomPrincipal : IPrincipal
     {
+        WindowsIdentity identity = null;
+        public CustomPrincipal(WindowsIdentity windowsIdentity)
+        {
+            identity = windowsIdentity;
+        }
+
+        public IIdentity Identity
+        {
+            get { return identity; }
+        }
+
+        public bool IsInRole(string permission)
+        {
+            foreach (IdentityReference group in this.identity.Groups)
+            {
+                SecurityIdentifier sid = (SecurityIdentifier)group.Translate(typeof(SecurityIdentifier));
+                var name = sid.Translate(typeof(NTAccount));
+                string groupName = Formatter.ParseName(name.ToString());
+                string[] permissions;
+                if (RolesConfig.GetPermissions(groupName, out permissions))
+                {
+                    if (permissions.Contains(permission))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
--- SecurityManager/RolesConfig.cs
+++ SecurityManager/RolesConfig.cs
@@ -9,5 +9,20 @@ namespace SecurityManager
 {
     internal class RolesConfig
     {
+        static string path = @"~\..\..\..\..\SecurityManager\RolesConfigFile.resx";
+        public static bool GetPermissions(string rolename, out string[] permissions)
+        {
+            permissions = new string[10];
+            string permissionString = string.Empty;
+
+            permissionString = (string)RolesConfigFile.ResourceManager.GetObject(rolename);
+            if (permissionString != null)
+            {
+                permissions = permissionString.Split(',');
+                return true;
+            }
+            return false;
+
+        }
     }
 }
