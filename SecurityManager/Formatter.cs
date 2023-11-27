9296f722a443394b2b55db8e903e96cbca21ba93
Katarina Marinkovic <ninikaca@gmail.com>
2023-11-27 20:28:30 +0100

    custom formatter

--- SecurityManager/Formatter.cs
+++ SecurityManager/Formatter.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityManager
 {
-    internal class Formatter
+    public class Formatter
     {
+        
+        // Returns username based on the Windows Logon Name. 
+        public static string ParseName(string winLogonName)
+        {
+            string[] parts = new string[] { };
+
+            if (winLogonName.Contains("@"))
+            {
+                ///UPN format
+                parts = winLogonName.Split('@');
+                return parts[0];
+            }
+            else if (winLogonName.Contains("\\"))
+            {
+                /// SPN format
+                parts = winLogonName.Split('\\');
+                return parts[1];
+            }
+            else
+            {
+                return winLogonName;
+            }
+        }
     }
 }
