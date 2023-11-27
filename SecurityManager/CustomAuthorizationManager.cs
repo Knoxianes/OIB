c9b6621404314a19b8cc9ac8f239bd3b868ea015
Katarina Marinkovic <ninikaca@gmail.com>
2023-11-27 21:06:13 +0100

    check access

--- SecurityManager/CustomAuthorizationManager.cs
+++ SecurityManager/CustomAuthorizationManager.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SecurityManager
 {
-    public class CustomAuthorizationManager
+    public class CustomAuthorizationManager : ServiceAuthorizationManager
     {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            CustomPrincipal principal = operationContext.ServiceSecurityContext.
+                 AuthorizationContext.Properties["Principal"] as CustomPrincipal;
+            return principal.IsInRole("Basic");
+        }
     }
 }
